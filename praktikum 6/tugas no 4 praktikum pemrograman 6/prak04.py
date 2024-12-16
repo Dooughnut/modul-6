27f5ecc24ee600a3a7a252d77835ead8e00b1d20
Dooughnut <kusotori25@gmail.com>
2024-12-17 00:46:44 +0800

    Add files via upload

---
+++ praktikum 6/tugas no 4 praktikum pemrograman 6/prak04.py
@@ -0,0 +1,29 @@
+kode=input()
+pesan=input()
+if len(kode)!=len(pesan):
+    print("Panjang Kalimat berbeda, pesan palsu")
+else :
+    bintang=0
+    pagar=0
+    hasil=""
+
+    for i,j in zip(kode,pesan):
+        if i==" " and j==" ":
+            continue
+
+        if i==j:
+            hasil +="*"
+            bintang +=1
+
+        else :
+            hasil+="#"
+            pagar +=1
+
+    print(hasil)
+    print(f"*={bintang}")
+    print(f"#={pagar}")
+
+    if bintang >= pagar:
+        print("Pesan Asli")
+    else:
+        print("Pesan Palsu")
